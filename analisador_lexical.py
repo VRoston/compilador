185683f352f7a20866727b828591398718b6e0b5
EduardoSilvaS <Eduardoss2020.ve@gmail.com>
2025-09-02 22:47:16 -0300

    feat: enhance lexical analyzer with file handling and token generation

--- analisador_lexical.py
+++ analisador_lexical.py
@@ -1,36 +1,62 @@
-import re
+pos = 0
 
 class Token:
-    def __init__(self, lex, symbol):
-        self.lexema = lex
-        self.simbolo = symbol
+    def __init__(self, lexema, simbolo):
+        self.lexema = lexema
+        self.simbolo = simbolo
 
-def __main__():
-  arquivo = read_file('./compilador/test.txt')
-  if arquivo is None:
-      return
+def main():
+    try:
+        nome_arquivo_entrada = './compilador/test.txt'
+        nome_arquivo_saida = "tabela_simbolos.txt"
 
-  pos = 0
-  tamanho_arquivo = len(arquivo)
+        with open(nome_arquivo_entrada, 'r') as file, open(nome_arquivo_saida, 'w') as out:
+            print("Arquivo de entrada aberto com sucesso!")
+            
+            out.write("Tabela de Símbolos:\n")
+            out.write(f"{'Lexema':<20} | {'Simbolo':<20}\n")
+            out.write("-" * 20 + "-+-" + "-" * 21 + "\n")
+ 
+            while True:
+                char = file.read(1)
+ 
+                if not char:
+                    break
 
-  while pos < tamanho_arquivo:
-      char_atual = arquivo[pos]
+                if char.isspace():
+                    continue
 
-      if char_atual in (' ', '\n', '\t', '\b'):
-          pos += 1
+                if char == '{':
+                    while char != '}' and char:
+                        char = file.read(1)
 
-      elif char_atual == '{':
-          pos += 1 
+                token = get_token(char, file)
+     
+                out.write(f"{token.lexema:<20} | {token.simbolo:<20}\n")
 
-          while pos < tamanho_arquivo and arquivo[pos] != '}':
-              pos += 1
+            print(f"Análise concluída! Tabela de símbolos salva em '{nome_arquivo_saida}'.")
 
-          if pos < tamanho_arquivo and arquivo[pos] == '}':
-              pos += 1
-          
-      else:
-          pos += 1
+    except FileNotFoundError:
+        print(f"Erro: O arquivo '{nome_arquivo_entrada}' não foi encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro inesperado: {e}")
 
-  print("Fim do arquivo")
-#
-__main__()
+    print("Fim do arquivo")
+
+def get_token(char, file):
+    if isdigit(char):
+        return handdle_digit(char, file)
+
+def handdle_digit(char, file):
+    lexema = char
+    char = file.read(1)
+    while char.isdigit():
+        lexema += char
+        char = file.read(1)
+    global pos
+    pos += len(lexema)
+    file.seek(pos)
+    return Token(lexema, "num")
+
+if __name__ == "__main__":
+    main()
