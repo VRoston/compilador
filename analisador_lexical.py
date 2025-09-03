51f198e73ccf0bdc9ede8a1c2bb28e897482f687
EduardoSilvaS <Eduardoss2020.ve@gmail.com>
2025-09-03 20:12:42 -0300

    feat: expand lexical analyzer with keyword recognition and enhanced token handling

--- analisador_lexical.py
+++ analisador_lexical.py
@@ -1,9 +1,31 @@
-pos = 0
-
 class Token:
     def __init__(self, lexema, simbolo):
         self.lexema = lexema
         self.simbolo = simbolo
+
+KEYWORDS = {
+    "programa": "sprograma",
+    "se": "sse",
+    "entao": "sentao",
+    "senao": "ssenao",
+    "enquanto": "senquanto",
+    "faca": "sfaca",
+    "inicio": "sinicio",
+    "fim": "sfim",
+    "escreva": "sescreva",
+    "leia": "sleia",
+    "var": "svar",
+    "inteiro": "sinteiro",
+    "booleano": "sbooleano",
+    "verdadeiro": "sverdadeiro",
+    "falso": "sfalso",
+    "procedimento": "sprocedimento",
+    "funcao": "sfuncao",
+    "div": "sdiv",
+    "e": "se",
+    "ou": "sou",
+    "nao": "snao",
+}
 
 def main():
     try:
@@ -44,19 +66,121 @@ def main():
     print("Fim do arquivo")
 
 def get_token(char, file):
-    if isdigit(char):
-        return handdle_digit(char, file)
+    if char.isdigit():
+        return handle_digit(char, file)
+    elif char.isalpha():
+        return handle_identifier_or_keyword(char, file)
+    elif char == ':':
+        return handle_assignment(char, file)
+    elif char in ['+', '-', '*']:
+        return handle_arithmetic_operator(char)
+    elif char in ['<', '>', '=', '!']:
+        return handle_relational_operator(char, file)
+    elif char in ['.', ';', ',', '(', ')']:
+        return handle_punctuation(char)
+    else:
+        return Token(char, "serro")
 
-def handdle_digit(char, file):
+def handle_punctuation(char):
     lexema = char
-    char = file.read(1)
-    while char.isdigit():
-        lexema += char
-        char = file.read(1)
-    global pos
-    pos += len(lexema)
-    file.seek(pos)
-    return Token(lexema, "num")
+    simbolo_map = {
+        '.': 'sponto',
+        ';': 'sponto_virgula',
+        ',': 'svirgula',
+        '(': 'sabre_parenteses',
+        ')': 'sfecha_parenteses'
+    }
+    simbolo = simbolo_map.get(char, 'serro')
+    return Token(lexema, simbolo)
+    
+def handle_relational_operator(char, file):
+    lexema = char
+    prox_char = file.read(1)
+    if char == '<':
+        if prox_char == '=':
+            lexema += prox_char
+            return Token(lexema, "smenorigual")
+        elif prox_char == '>':
+            lexema += prox_char
+            return Token(lexema, "sdif")
+        else:
+            if prox_char:
+                file.seek(file.tell() - 1)
+            return Token(lexema, "smenor")
+    elif char == '>':
+        if prox_char == '=':
+            lexema += prox_char
+            return Token(lexema, "smaiorigual")
+        else:
+            if prox_char:
+                file.seek(file.tell() - 1)
+            return Token(lexema, "smaior")
+    elif char == '=':
+        if prox_char == '=':
+            lexema += prox_char
+            return Token(lexema, "sigual")
+        else:
+            if prox_char:
+                file.seek(file.tell() - 1)
+            return Token(lexema, "serro")
+    elif char == '!':
+        if prox_char == '=':
+            lexema += prox_char
+            return Token(lexema, "sdif")
+        else:
+            if prox_char:
+                file.seek(file.tell() - 1)
+            return Token(lexema, "serro")
+    else:
+        return Token(lexema, "serro")
+
+def handle_arithmetic_operator(char):
+    lexema = char
+    simbolo_map = {
+        '+': 'smais',
+        '-': 'smenos',
+        '*': 'smult'
+    }
+    simbolo = simbolo_map.get(char, 'serro')
+    return Token(lexema, simbolo)
+
+def handle_assignment(char, file):
+    lexema = char
+    prox_char = file.read(1)
+    if prox_char == '=':
+        lexema += prox_char
+        return Token(lexema, "satribuicao")
+    else:
+        if prox_char:
+            file.seek(file.tell() - 1)
+        return Token(lexema, "sdoispontos")
+
+
+def handle_identifier_or_keyword(char, file):
+    lexema = char
+    while True:
+        prox_char = file.read(1)
+        if prox_char.isalnum() or prox_char == '_':
+            lexema += prox_char
+        else:
+            if prox_char:
+                file.seek(file.tell() - 1)
+            break
+
+    simbolo = KEYWORDS.get(lexema, "sidentificador")
+    return Token(lexema, simbolo)
+
+def handle_digit(char, file):
+    lexema = char
+    while True:
+        prox_char = file.read(1)
+        if prox_char.isdigit():
+            lexema += prox_char
+        else:
+            if prox_char:
+                file.seek(file.tell() - 1)
+            break
+    return Token(lexema, "snumero")
 
 if __name__ == "__main__":
     main()
